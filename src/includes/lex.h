b3c2ef74acd0f28984a703921cf5b5438efccfda
drkwitht <derektan3510@gmail.com>
2026-05-04 19:49:38 -0700

    Add initial toy lang lexer.

---
+++ src/includes/lex.h
@@ -0,0 +1,102 @@
+#ifndef TOYSCRIPT_LEX_H
+#define TOYSCRIPT_LEX_H
+
+#include "mystr.h"
+
+typedef enum token_tag_t {
+    tk_unknown,
+    tk_spaces,
+    tk_comment,
+    tk_keyword_let,
+    tk_keyword_if,
+    tk_keyword_else,
+    tk_keyword_while,
+    tk_keyword_ret,
+    tk_keyword_fun,
+    tk_identifier,
+    tk_none,
+    tk_true,
+    tk_false,
+    tk_integer,
+    tk_real,
+    tk_os_times,
+    tk_os_slash,
+    tk_os_plus,
+    tk_os_minus,
+    tk_os_bang,
+    tk_os_equals,
+    tk_os_bang_equals,
+    tk_os_lesser,
+    tk_os_greater,
+    tk_comma,
+    tk_colon,
+    tk_semicolon,
+    tk_lparen,
+    tk_rparen,
+    tk_eof
+} TkTag;
+
+typedef struct token_v_t {
+    int begin;
+    int length;
+    int line;
+    TkTag tag;
+} Token;
+
+static inline int8_t is_word_symbol(char c) {
+    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_';
+}
+
+static inline int8_t is_numeric_symbol(char c) {
+    return (c >= '0' && c <= '9') || c == '.';
+}
+
+static inline int8_t is_op_symbol(char c) {
+    switch (c) {
+        case '*': case '/': case '+': case '-': // arithmetic
+        case '=': case '!': case '<': case '>': // comparison / logical
+        case '?': return 1; // extra
+        default: return 0;
+    }
+}
+
+static inline int8_t is_space_symbol(char c) {
+    switch (c) {
+        case ' ': case '\t': case '\n': return 1;
+        default: return 0;
+    }
+}
+
+typedef struct lexical_item_t {
+    const char *literal;
+    TkTag tag;
+} LexItem;
+
+typedef struct lexer_t {
+    const LexItem *specials;
+    int pos;
+    int end;
+    int line;
+} Lexer;
+
+Lexer make_lexer(const charspan *s, const LexItem *special_array);
+
+int8_t lexer_done(const Lexer *self);
+
+void lexer_consume(Lexer *self, char c);
+
+Token lexer_lex_space(Lexer *self, const charspan *s);
+
+Token lexer_lex_single(Lexer *self, TkTag tag, const charspan *s);
+
+Token lexer_lex_between(Lexer *self, TkTag tag, const charspan *s);
+
+Token lexer_lex_numeric(Lexer *self, const charspan *s);
+
+Token lexer_lex_word(Lexer *self, const charspan *s);
+
+Token lexer_lex_operator(Lexer *self, const charspan *s);
+
+Token lexer_next(Lexer *self, const charspan *s);
+
+#endif
---
+++ src/lex.c
@@ -0,0 +1,226 @@
+#include <string.h>
+#include "lex.h"
+
+Lexer make_lexer(const charspan *s, const LexItem *special_array) {
+    return (Lexer) {
+        .specials = special_array,
+        .pos = 0,
+        .end = charspan_len(s),
+        .line = 1
+    };
+}
+
+int8_t lexer_done(const Lexer *self) {
+    return self->pos >= self->end;
+}
+
+void lexer_consume(Lexer *self, char c) {
+    if (c == '\n') {
+        self->line++;
+    }
+
+    self->pos++;
+}
+
+Token lexer_lex_space(Lexer *self, const charspan *s) {
+    const int tk_start = self->pos;
+    const int tk_line = self->line;
+
+    while (!lexer_done(self)) {
+        const char c = s->data[self->pos];
+
+        if (is_space_symbol(c)) {
+            lexer_consume(self, c);
+        } else {
+            break;
+        }
+    }
+
+    return (Token) {
+        .begin = tk_start,
+        .length = self->pos - tk_start,
+        .line = tk_line,
+        .tag = tk_spaces
+    };
+}
+
+Token lexer_lex_single(Lexer *self, TkTag tag, const charspan *s) {
+    const int tk_start = self->pos;
+    const int tk_line = self->line;
+
+    lexer_consume(self, s->data[tk_start]);
+
+    return (Token) {
+        .begin = tk_start,
+        .length = 1,
+        .line = tk_line,
+        .tag = tag
+    };
+}
+
+Token lexer_lex_between(Lexer *self, TkTag tag, const charspan *s) {
+    const char delim = s->data[self->pos];
+    lexer_consume(self, delim);
+
+    const int tk_start = self->pos;
+    const int tk_line = self->line;
+    int8_t closed = 0;
+
+    while (!lexer_done(self)) {
+        const char c = s->data[self->pos];
+
+        if (c == delim) {
+            lexer_consume(self, c);
+            closed = 1;
+
+            break;
+        } else {
+            lexer_consume(self, c);
+        }
+    }
+
+    return (Token) {
+        .begin = tk_start,
+        .length = self->pos - tk_start - 1,
+        .line = tk_line,
+        .tag = (closed) ? tag : tk_unknown
+    };
+}
+
+Token lexer_lex_numeric(Lexer *self, const charspan *s) {
+    const int tk_start = self->pos;
+    const int tk_line = self->line;
+    int8_t points = 0;
+
+    while (!lexer_done(self)) {
+        const char c = s->data[self->pos];
+
+        if (is_numeric_symbol(c)) {
+            if (c == '.') points++;
+
+            lexer_consume(self, c);
+        } else {
+            break;
+        }
+    }
+
+    TkTag temp_tag;
+
+    if (points == 0) {
+        temp_tag = tk_integer;
+    } else if (points == 1) {
+        temp_tag = tk_real;
+    } else {
+        temp_tag = tk_unknown;
+    }
+
+    return (Token) {
+        .begin = tk_start,
+        .length = self->pos - tk_start,
+        .line = tk_line,
+        .tag = temp_tag
+    };
+}
+
+Token lexer_lex_word(Lexer *self, const charspan *s) {
+    const int tk_start = self->pos;
+    const int tk_line = self->line;
+
+    while (!lexer_done(self)) {
+        const char c = s->data[self->pos];
+
+        if (is_word_symbol(c)) {
+            lexer_consume(self, c);
+        } else {
+            break;
+        }
+    }
+
+    const int tk_length = self->pos - tk_start;
+    TkTag temp_tag = tk_unknown;
+
+    for (const LexItem *special_it = self->specials; special_it->literal != NULL; special_it++) {
+        charspan lexeme;
+        charspan_new(&lexeme, s->data + tk_start, tk_length);
+
+        if (charspan_equals_raw(&lexeme, special_it->literal, tk_length)) {
+            temp_tag = special_it->tag;
+        }
+    }
+
+    return (Token) {
+        .begin = tk_start,
+        .length = tk_length,
+        .line = tk_line,
+        .tag = temp_tag
+    };
+}
+
+Token lexer_lex_operator(Lexer *self, const charspan *s) {
+    const int tk_start = self->pos;
+    const int tk_line = self->line;
+
+    while (!lexer_done(self)) {
+        const char c = s->data[self->pos];
+
+        if (is_op_symbol(c)) {
+            lexer_consume(self, c);
+        } else {
+            break;
+        }
+    }
+
+    const int tk_length = self->pos - tk_start;
+    TkTag temp_tag = tk_unknown;
+
+    for (const LexItem *special_it = self->specials; special_it->literal != NULL; special_it++) {
+        charspan lexeme;
+        charspan_new(&lexeme, s->data + tk_start, tk_length);
+
+        if (charspan_equals_raw(&lexeme, special_it->literal, tk_length)) {
+            temp_tag = special_it->tag;
+        }
+    }
+
+    return (Token) {
+        .begin = tk_start,
+        .length = tk_length,
+        .line = tk_line,
+        .tag = temp_tag
+    };
+}
+
+Token lexer_next(Lexer *self, const charspan *s) {
+    if (lexer_done(self)) {
+        return (Token) {
+            .begin = self->end,
+            .length = 1,
+            .line = self->line,
+            .tag = tk_eof
+        };
+    }
+
+    const char c = s->data[self->pos];
+
+    switch (c) {
+        case ',': return lexer_lex_single(self, tk_comma, s);
+        case ':': return lexer_lex_single(self, tk_colon, s);
+        case ';': return lexer_lex_single(self, tk_semicolon, s);
+        case '(': return lexer_lex_single(self, tk_lparen, s);
+        case ')': return lexer_lex_single(self, tk_rparen, s);
+        case '#': return lexer_lex_between(self, tk_comment, s);
+        default: break;
+    }
+
+    if (is_space_symbol(c)) {
+        return lexer_lex_space(self, s);
+    } else if (is_numeric_symbol(c)) {
+        return lexer_lex_numeric(self, s);
+    } else if (is_op_symbol(c)) {
+        return lexer_lex_operator(self, s);
+    } else if (is_word_symbol(c)) {
+        return lexer_lex_word(self, s);
+    }
+
+    return lexer_lex_single(self, tk_unknown, s);
+}
