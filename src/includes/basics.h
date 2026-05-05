3b2e0b4ed0da889ba6e98ee0e52e6f62b6aaa85a
drkwitht <derektan3510@gmail.com>
2026-05-04 19:49:25 -0700

    Add macro-generic vec, usage todo.

---
+++ src/includes/basics.h
@@ -0,0 +1,27 @@
+#ifndef TOYSCRIPT_BASICS_H
+#define TOYSCRIPT_BASICS_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define FATAL_ABORT(title, file, line, msg)\
+fprintf(stderr, "Error at %s:%d\n%s: %s\n", file, line, title, msg);\
+exit(1);\
+
+// ! NOTE: Only pass __FILE__ or __LINE__ for file and line respectively when using this macro.
+
+#define DUD_OBJS_N(type, name_of_p, n, file, line)\
+name_of_p = calloc((size_t)n, sizeof(type));\
+if (name_of_p == NULL) {\
+    FATAL_ABORT("Alloc Error", file, line, "Failed to initialize buffer of objects.");\
+}\
+for (size_t name_of_p##_##i = 0; name_of_p##_##i < n; name_of_p##_##i++) {\
+    type##_dud(name_of_p + name_of_p##_##i);\
+}\
+\
+
+#define NEW_OBJ(type, name, args...) type##_new(&name, args)
+#define DELETE_OBJ(type, name) type##_del(&name)
+
+#endif
---
+++ src/includes/vec.h
@@ -0,0 +1,149 @@
+#ifndef TOYSCRIPT_VEC_H
+#define TOYSCRIPT_VEC_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include "basics.h"
+
+#define ANYVEC_DEFAULT_SIZE 8
+
+#define STUB_VEC(type)\
+typedef struct ts_any_vec_t {\
+    type *data;\
+    size_t length;\
+    size_t capacity;\
+} AnyVec##_##type;\
+\
+void AnyVec##_##type##_dud(AnyVec##_##type *self);\
+void AnyVec##_##type##_new(AnyVec##_##type *self, size_t n, const type *value);\
+void AnyVec##_##type##_copy(AnyVec##_##type *self, const AnyVec##_##type *other);\
+void AnyVec##_##type##_move(AnyVec##_##type *self, AnyVec##_##type *x);\
+void AnyVec##_##type##_del(AnyVec##_##type *self);\
+int8_t AnyVec##_##type##_empty(AnyVec##_##type *self);\
+size_t AnyVec##_##type##_len(AnyVec##_##type *self);\
+const type *AnyVec##_##type##_get(const AnyVec##_##type *self, size_t pos);\
+type *AnyVec##_##type##_getm(AnyVec##_##type *self, size_t pos);\
+void AnyVec##_##type##_push(AnyVec##_##type *self, const type *value);\
+void AnyVec##_##type##_pop(AnyVec##_##type *self);\
+\
+
+#define IMPL_VEC(type)\
+void AnyVec##_##type##_dud(AnyVec##_##type *self) {\
+    type *temp = NULL;\
+    DUD_OBJS_N(type, temp, ANYVEC_DEFAULT_SIZE, __FILE__, __LINE__);\
+    \
+    self->data = temp;\
+    self->length = 0;\
+    self->capacity = ANYVEC_DEFAULT_SIZE;\
+}\
+\
+void AnyVec##_##type##_new(AnyVec##_##type *self, size_t n, const type *value) {\
+    type *temp = NULL;\
+    DUD_OBJS_N(type, temp, n, __FILE__, __LINE__);\
+    \
+    for (size_t filler_i = 0; filler_i < n; filler_i++) {\
+        type##_##copy(temp + filler_i, value);\
+    }\
+    \
+    self->data = temp;\
+    self->length = 0;\
+    self->capacity = n;\
+}\
+\
+void AnyVec##_##type##_copy(AnyVec##_##type *self, const AnyVec##_##type *other) {\
+    const size_t other_count = other->length;\
+    const size_t other_capacity = other->capacity;\
+    \
+    type *temp = NULL;\
+    DUD_OBJS_N(type, temp, other_capacity, __FILE__, __LINE__);\
+    \
+    for (size_t copy_i = 0; copy_i < other_count; copy_i++) {\
+        type##_copy(temp + copy_i, other->data + copy_i);\
+    }\
+    \
+    self->data = temp;\
+    self->length = other->length;\
+    self->capacity = other->capacity;\
+}\
+\
+void AnyVec##_##type##_move(AnyVec##_##type *self, AnyVec##_##type *x) {\
+    if (self == x) {\
+        return;\
+    }\
+    \
+    if (AnyVec##_##type##_empty(self) == 0) {\
+        AnyVec##_##type##_del(self);\
+    }\
+    \
+    self->data = x->data;\
+    x->data = NULL;\
+    self->length = x->length;\
+    x->length = 0;\
+    self->capacity = x->capacity;\
+    x->capacity = 0;\
+}\
+\
+void AnyVec##_##type##_del(AnyVec##_##type *self) {\
+    if (self->data != NULL) {\
+        type *temp = self->data;\
+        for (size_t destroy_i = 0; destroy_i < self->length; destroy_i++) {\
+            type##_del(temp + destroy_i);\
+        }\
+        self->length = 0;\
+        self->capacity = 0;\
+    }\
+}\
+\
+int8_t AnyVec##_##type##_empty(AnyVec##_##type *self) {\
+    return self->data != NULL || self->length == 0;\
+}\
+\
+size_t AnyVec##_##type##_len(AnyVec##_##type *self) {\
+    return self->length;\
+}\
+\
+const type *AnyVec##_##type##_get(const AnyVec##_##type *self, size_t pos) {\
+    if (pos >= self->length) {\
+        FATAL_ABORT("OOB Error", __FILE__, __LINE__, "OOB access of AnyVec here!")\
+    }\
+    \
+    return self->data + pos;\
+}\
+\
+type *AnyVec##_##type##_getm(AnyVec##_##type *self, size_t pos) {\
+    if (pos >= self->length) {\
+        FATAL_ABORT("OOB Error", __FILE__, __LINE__, "OOB access of AnyVec here!")\
+    }\
+    \
+    return self->data + pos;\
+}\
+\
+void AnyVec##_##type##_push(AnyVec##_##type *self, const type *value) {\
+    const size_t next_pos = self->length;\
+    const size_t old_capacity = self->capacity;\
+    \
+    if (next_pos >= old_capacity) {\
+        const size_t new_capacity = (old_capacity * 3) / 2;\
+        type *temp_p = realloc(self->data, sizeof(type) * new_capacity);\
+        \
+        if (temp_p == NULL) {\
+            FATAL_ABORT("Realloc Error", __FILE__, __LINE__, "Failed to reallocate AnyVec buffer here!");\
+        }\
+        self->data = temp_p;\
+        self->capacity = new_capacity;\
+    }\
+    \
+    self->data[next_pos] = *value;\
+    self->length++;\
+}\
+\
+void AnyVec##_##type##_pop(AnyVec##_##type *self) {\
+    const size_t next_pos = self->length - 1;\
+    \
+    type##_del(self->data + next_pos);\
+    type##_dud(self->data + next_pos);\
+    self->length--;\
+}\
+\
+
+#endif
