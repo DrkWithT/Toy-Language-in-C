1cae4ffbdc53a4e5d320ee62629bdab4a59186d2
drkwitht <derektan3510@gmail.com>
2026-05-05 11:57:46 -0700

    Add 1st bytecode repr.

---
+++ src/bytecode.c
@@ -0,0 +1,101 @@
+#include "bytecode.h"
+
+static const char *opcode_names[] = {
+    "nop",
+    "op_put_none",
+    "op_put_bool",
+    "op_load_imm_gid",
+    "op_load_local",
+    "op_store_local",
+    "op_put_konst",
+    "op_dup",
+    "op_mul",
+    "op_div",
+    "op_add",
+    "op_sub",
+    "op_eq",
+    "op_ne",
+    "op_lt",
+    "op_gt",
+    "op_jmp",
+    "op_jmp_false",
+    "op_call",
+    "op_put_callee",
+    "op_ret"
+};
+
+void Instruction_dud(Instruction* ins) {}
+void Instruction_new(MAYBE_UNUSED Instruction *ins) {}
+void Instruction_copy(MAYBE_UNUSED Instruction *ins, MAYBE_UNUSED const Instruction *other) {}
+void Instruction_move(MAYBE_UNUSED Instruction *ins, MAYBE_UNUSED Instruction *other) {}
+void Instruction_del(MAYBE_UNUSED Instruction *ins) {}
+
+IMPL_VEC(Instruction)
+
+
+
+void Chunk_dud(Chunk* c) {
+    AnyVec_Instruction_dud(&c->code);
+    AnyVec_Value_dud(&c->constants);
+}
+
+void Chunk_new(Chunk *c) {
+    AnyVec_Instruction_dud(&c->code);
+    AnyVec_Value_dud(&c->constants);
+}
+
+void Chunk_copy(Chunk *c, const Chunk *other) {
+    AnyVec_Instruction_copy(&c->code, &other->code);
+    AnyVec_Value_copy(&c->constants, &other->constants);
+}
+
+void Chunk_move(Chunk *c, Chunk *other) {
+    AnyVec_Instruction_move(&c->code, &other->code);
+    AnyVec_Value_move(&c->constants, &other->constants);
+}
+
+void Chunk_del(MAYBE_UNUSED Chunk *c) {
+    AnyVec_Instruction_del(&c->code);
+    AnyVec_Value_del(&c->constants);
+}
+
+const Instruction *Chunk_code(const Chunk *c) {
+    return c->code.data;
+}
+
+const Value *Chunk_constants(const Chunk *c) {
+    return c->constants.data;
+}
+
+void dump_program(const Program *pg) {
+    const size_t pg_chunks_n = AnyVec_Chunk_len(&pg->chunks);
+
+    puts("---- BYTECODE DUMP ----\n");
+
+    for (size_t chunk_pos = 0; chunk_pos < pg_chunks_n; chunk_pos++) {
+        printf("CHUNK %zu:\n", chunk_pos);
+
+        const Chunk *temp_chunk = AnyVec_Chunk_get(&pg->chunks, chunk_pos);
+        const size_t temp_chunk_const_n = AnyVec_Value_len(&pg->chunks.data->constants);
+        const size_t temp_chunk_code_n = AnyVec_Instruction_len(&pg->chunks.data->code);
+
+        puts("CONSTANTS:\n");
+
+        for (size_t temp_chunk_const_id = 0; temp_chunk_const_id < temp_chunk_const_n; temp_chunk_const_id++) {
+            printf("const%zu = ", temp_chunk_const_id);
+            print_value(AnyVec_Value_get(&pg->chunks.data->constants, temp_chunk_const_id));
+            printf("\n");
+        }
+
+        puts("CODE:\n");
+
+        for (size_t temp_chunk_code_id = 0; temp_chunk_code_id < temp_chunk_code_n; temp_chunk_code_id++) {
+            const Instruction *ins = AnyVec_Instruction_get(&pg->chunks.data->code, temp_chunk_code_id);
+            printf("%zu: %s   %d, %d\n", temp_chunk_code_id, opcode_names[ins->op], ins->flag, ins->wide);
+        }
+
+        printf("\n\n\n");
+    }
+}
+
+IMPL_VEC(Chunk)
---
+++ src/includes/bytecode.h
@@ -0,0 +1,75 @@
+#ifndef TOYSCRIPT_BYTECODE_H
+#define TOYSCRIPT_BYTECODE_H
+
+#include <stdint.h>
+#include "value.h"
+#include "vec.h"
+
+
+
+typedef struct code_vec_t Instruction;
+
+void Instruction_dud(Instruction* ins);
+void Instruction_new(MAYBE_UNUSED Instruction *ins);
+void Instruction_copy(MAYBE_UNUSED Instruction *ins, MAYBE_UNUSED const Instruction *other);
+void Instruction_move(MAYBE_UNUSED Instruction *ins, MAYBE_UNUSED Instruction *other);
+void Instruction_del(MAYBE_UNUSED Instruction *ins);
+
+typedef struct code_chunk_t Chunk;
+
+void Chunk_dud(Chunk* c);
+void Chunk_new(Chunk *c);
+void Chunk_copy(Chunk *c, const Chunk *other);
+void Chunk_move(Chunk *c, Chunk *other);
+void Chunk_del(Chunk *c);
+
+const Instruction *Chunk_code(const Chunk *c);
+const Value *Chunk_constants(const Chunk *c);
+
+STUB_VEC(Instruction)
+
+STUB_VEC(Chunk)
+
+typedef enum vm_opcode_t : uint8_t {
+    op_nop,
+    op_put_none,
+    op_put_bool,
+    op_load_imm_gid,    // ? loads an immediate procedure ID --> chunk ID to dispatch to.
+    op_load_local,
+    op_store_local,
+    op_put_k,
+    op_dup,
+    op_mul,
+    op_div,
+    op_add,
+    op_sub,
+    op_eq,
+    op_ne,
+    op_lt,
+    op_gt,
+    op_jmp,
+    op_jmp_false,
+    op_call,
+    op_put_callee,
+    op_ret
+} Opcode;
+
+typedef struct code_vec_t {
+    Opcode op;
+    uint8_t flag;
+    uint16_t wide;
+} Instruction;
+
+typedef struct code_chunk_t {
+    AnyVec_Instruction code;
+    AnyVec_Value constants;
+} Chunk;
+
+typedef struct vm_program_t {
+    AnyVec_Chunk chunks;
+    int entry_id;
+} Program;
+
+void dump_program(const Program *pg);
+
+#endif
