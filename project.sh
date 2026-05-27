24109e439a5f23a80628b5390e4dc76d64689615
drkwitht <derektan3510@gmail.com>
2026-05-27 13:37:46 -0700

    Update build setup for shared lib demo.

--- project.sh
+++ project.sh
@@ -14,6 +14,12 @@ elif [[ $mode = "br" ]]; then
 elif [[ $mode = "bp" ]]; then
     cmake -S . -B build -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo && cmake --build build;
     mv ./build/compile_commands.json .;
+elif [[ $mode = "dd" ]]; then
+    cmake -S . -B build -DCMAKE_BUILD_TYPE:STRING=Debug && cmake --build build;
+    mv ./build/compile_commands.json .;
+elif [[ $mode = "dr" ]]; then
+    cmake -S . -B build -DCMAKE_BUILD_TYPE:STRING=Release -DCREATE_TBASIC_SHARED && cmake --build build;
+    mv ./build/compile_commands.json .;
 else
-    printf "Usage:\n\tbd: build debug\n\tbr: build release\n\tbp: build profile";
+    printf "Usage:\n\tbd: build debug\n\tbr: build release\n\tbp: build profile\n\tdd: build shared lib & demo (debug)\n\tdr: build shared lib & demo (release)\n";
 fi
